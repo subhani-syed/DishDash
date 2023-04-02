1a50fc3c071f85f317c4320d09dd8c445ee91873
Subhani-syed <subhani6352@gmail.com>
2023-04-02 10:32:38 +0530

    Implement search functionality

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,14 +1,38 @@
 import Card from "./Card";
 import { restaurantList } from "../config";
+import { useState } from "react";
 
 const Body = () => {
-    return (
+  const [searchText,setsearchText] = useState("");
+  const [cardlist,setCardList] = useState(restaurantList);
+
+  function search(text){
+    const filteredData = restaurantList.filter((res)=>{
+      return res?.data?.name.toLowerCase().includes(text.toLowerCase())
+    })
+    return filteredData;
+  }
+
+  
+  return (
+    <>
+      <input type="text" placeholder="Search Restaurant" value={searchText} onChange={(e)=>{
+        setsearchText(e.target.value);
+      }}/>
+
+      <button onClick={()=>{
+        const res = search(searchText);
+        setCardList(res);
+      }}>Search</button>
+
       <div className="container">
-          {restaurantList.map((res)=>{
+          {cardlist.map((res)=>{
               return <Card {...res.data} key={res.data.uuid}/>
           })}
       </div>
-    );
-  };
+
+    </>
+  );
+};
 
 export default Body;
