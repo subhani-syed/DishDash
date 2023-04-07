16966c44d2c8550bd81361dd2227118a62476224
Subhani-syed <subhani6352@gmail.com>
2023-04-07 14:35:33 +0530

    Fix key warning

--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -1,8 +1,8 @@
 const Shimmer = ()=>{
-    return <>
+    return(
         <div className="shimmer-main">
-            {new Array(12).fill(<div className="shimmer-card"></div>)}
+            {Array(12).fill("").map((e,index)=>(<div key={index} className="shimmer-card"></div>))}
         </div>
-    </>
+    )
 }
 export default Shimmer;
